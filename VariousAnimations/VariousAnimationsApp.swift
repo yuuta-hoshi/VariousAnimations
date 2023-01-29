71b1ecdc73b9cff9dca37303a25f29af26685c87
yuuta-hoshi <h_y0608@icloud.com>
2023-01-29 21:50:54 +0900

    Initial Commit

---
+++ VariousAnimations/VariousAnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  VariousAnimationsApp.swift
+//  VariousAnimations
+//
+//  Created by 星優大 on 2023/01/29.
+//
+
+import SwiftUI
+
+@main
+struct VariousAnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
